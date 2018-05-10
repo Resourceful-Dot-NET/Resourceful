7a1735516ea1c931e3bbfffb96f93768232a940c
John Louderback <john@powereddynamics.com>
2018-05-09 21:47:09 -0700

    Added WPF integration test app

---
+++ Resourceful.Wpf.TestApp/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace Resourceful.Wpf.TestApp {
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App {
+  }
+}
---
+++ Resourceful.Wpf.TestApp/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Resourceful.Wpf.TestApp {
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow {
+    public MainWindow() {
+      InitializeComponent();
+
+      ResourceManager.Default
+        .BindToEmbeddedResource("TestResources/test.txt",
+          res => Application.Current.Dispatcher.Invoke(() => Label.Text = res));
+    }
+  }
+}
