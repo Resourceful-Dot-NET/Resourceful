d20e165eee08de51474cb5bd68629ebeea566e8d
John Louderback <john.louderback@deckers.com>
2018-06-18 19:14:57 -0700

    Added ability to use Resourceful in .Net Framework 4.5

--- ResourcefulShared/Utilities.cs
+++ ResourcefulShared/Utilities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace ResourcefulShared {
@@ -6,9 +7,29 @@ namespace ResourcefulShared {
 
     #region Properties
 
+#if NET45
+    public static bool IsWindows {
+      get {
+        var windir = Environment.GetEnvironmentVariable("windir");
+        return (!string.IsNullOrEmpty(windir) && windir.Contains(@"\") && Directory.Exists(windir));
+      }
+    }
+
+    public static bool IsLinux {
+      get {
+        if (!File.Exists(@"/proc/sys/kernel/ostype")) return false;
+        var osType = File.ReadAllText(@"/proc/sys/kernel/ostype");
+        return osType.StartsWith("Linux", StringComparison.OrdinalIgnoreCase);
+      }
+    }
+
+    public static bool IsMacOS => File.Exists(@"/System/Library/CoreServices/SystemVersion.plist");
+
+#else
     public static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
     public static bool IsLinux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
     public static bool IsMacOS => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+#endif
 
     public static bool IsConsoleApp {
       get {
